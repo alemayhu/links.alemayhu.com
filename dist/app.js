1ed5f8773bee9941bdb4f0eecdacc33ca8749c4a
Alexander Alemayhu <alexander@alemayhu.com>
2021-01-21 22:40:57 +0100

    Remove less popular links

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
         const links = [
                 { title: "YouTube", link: "https://youtube.com/c/alexanderalemayhu" },
-                { title: "Minds", link: "https://www.minds.com/alexanderalemayhu/" },
                 { title: "Telegram", link: "https://t.me/joinchat/TkdYqYimUj-lp7JD" },
                 { title: "Tiktok", link: "https://www.tiktok.com/@alexanderalemayhu" },
-                { title: "Instagram", link: "https://instagram.com/alexanderalemayhu" },
                 { title: "Discord", link: "https://discord.gg/PSKC3uS" },
                 { title: "GitHub Sponsor", link: "http://github.com/sponsors/alemayhu" },
                 { title: "Patreon", link: "https://www.patreon.com/alemayhu" },
                 { title: "Website", link: "https://alemayhu.com" },
                 { title: "Twitter", link: "https://twitter.com/AAlemayhu" },
-                { title: "Facebook", link: "https://facebook.com/alexanderalemayhu1" },
                 { title: "Buy me a coffee", link: "https://buymeacoffee.com/aalemayhu" },
                 { title: "Tip", link: "https://streamlabs.com/alexanderalemayhu1" },
                 { title: "Ko-Fi", link: "https://ko-fi.com/alemayhu" },
